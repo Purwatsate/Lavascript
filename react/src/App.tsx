8e71f80b42c73b4835ea75e374c2bed32054603a
Aung Naing Tun <aungnaing.tun@partnerassociates.com>
2026-06-04 23:45:52 +0630

    added tailwind css

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World</h1>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
